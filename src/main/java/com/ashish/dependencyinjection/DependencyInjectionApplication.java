b5474633b2753518897f50a93b7d1f946b07f38a
Ashish Sharda <ashishjsharda@gmail.com>
2022-06-23 04:38:19 -0500

    Add RestTemplate

--- src/main/java/com/ashish/dependencyinjection/DependencyInjectionApplication.java
+++ src/main/java/com/ashish/dependencyinjection/DependencyInjectionApplication.java
@@ -1,13 +1,22 @@
 package com.ashish.dependencyinjection;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
+    @Autowired
+
     public static void main(String[] args) {
         SpringApplication.run(DependencyInjectionApplication.class, args);
+    }
+
+    public RestTemplate getRestTemplate()
+    {
+        return new RestTemplate();
     }
 
 }
